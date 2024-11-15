9f1e9efcb88f1680c52c0a2b82caeb44cb4c5960
허성원 <hurdang99@naver.com>
2024-11-15 14:27:18 +0900

    Modify all

--- src/textrptg/GameManager.java
+++ src/textrptg/GameManager.java
@@ -1,39 +1,63 @@
 package textrptg;
 
+import java.io.BufferedReader;
 import java.util.HashMap;
 import java.util.Map;
 
+import Stage.StageBattle;
+import Stage.StageLobby;
+import Stage.StageSelctJob;
+import Stage.StageSetting;
+import Stage.StageTitle;
 import Unit.Player;
 
 public class GameManager {
 
 	public static String nextStage;
-	public static Object reader;
+	public static BufferedReader reader;
 	public static Player player;
-	public static Object instance;
-	Map<String, Stage> stageList = new HashMap<String, Stage>();
+	public static GameManager instance;
+	Map<String, StageSetting> stageList = new HashMap<String, StageSetting>();
+	private StringBuilder buffer = new StringBuilder(); // buffer 선언
 
-	public abstract class Stage {
-		protected StringBuffer buffer = new StringBuffer();
+	private GameManager() {
 
-		public abstract boolean updateScreen();
 	}
 
-	void init() {
+	public static GameManager getInstance() {
+		if (instance == null) {
+			instance = new GameManager();
+		}
+		return instance;
+	}
+
+	public void init() {
 		stageList.put("TITLE", new StageTitle());
-		stageList.put("SELECTJAB", new StageSelctJob());
+		stageList.put("SELECTJOB", new StageSelctJob());
 		stageList.put("BATTLE", new StageBattle());
 		stageList.put("LOBBY", new StageLobby());
-		stageList.put("SETTING", new StageSetting());
 		nextStage = "TITLE";
 	}
 
 	public boolean updateScreen() {
-		Stage currentStage = stageList.get(nextStage);
+		StageSetting currentStage = stageList.get(nextStage);
 		if (currentStage != null) {
-			return currentStage.updateScreen(); 
+			return currentStage.updateScreen();
 		}
-		return false; 
+		return false;
+	}
+
+	public void changeStage(String stageName) {
+		if (stageList.containsKey(stageName)) {
+			nextStage = stageName;
+		} else {
+			buffer.setLength(0);
+			buffer.append("잘못된 스테이지 이름입니다.");
+		}
+	}
+
+	public static BufferedReader getReader() {
+		return reader;
 	}
 
 }
