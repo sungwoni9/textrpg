49d806cd9aa01d5cc9e6aa11f3ebc8ca9fd6e3c8
허성원 <hurdang99@naver.com>
2024-11-15 12:32:01 +0900

    Modify reader, writer and updateScreen method

--- src/Stage/StageTitle.java
+++ src/Stage/StageTitle.java
@@ -1,8 +1,16 @@
 package Stage;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 import textrptg.GameManager;
 
 public class StageTitle extends Stage {
+
+	private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	private static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
 	private static String input = "";
 
@@ -12,16 +20,23 @@ public class StageTitle extends Stage {
 		buffer.append("==== TEXT RPG ====\n");
 		buffer.append("[시작] 을 입력하세요\n");
 
-		System.out.println(buffer.toString());
-		
 		try {
-			input = buffer.toString();
+			writer.write(buffer.toString());
+			writer.flush();
+		} catch (Exception e) {
+			System.out.println("출력 오류가 발생했습니다.");
+		}
+
+		try {
+			String input = reader.readLine();
+
 			if (input.equals("시작")) {
 				GameManager.nextStage = "SELECTJAB";
 				return false;
 			}
 		} catch (Exception e) {
-			System.out.println();
+			System.out.println("다시 입력해주세요");
+
 		}
 		return true;
 	}
