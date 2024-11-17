6eed0bc46f138c891ba9b1ee9b0d68a977704096
sungwoni9 <hurdang99@naver.com>
2024-11-17 23:51:13 +0900

    Add : generateMonsters and addPlayer, removePlayer method

--- src/Unit/UnitManager.java
+++ src/Unit/UnitManager.java
@@ -9,15 +9,34 @@ public class UnitManager {
 	public static UnitManager instance = new UnitManager();
 
 	Random r = new Random();
-	Player player = new Player();
+	Player player = new Player(0, 0, 0, 0, 0, 0);
 
 	Vector<Player> player_list = new Vector<Player>();
 	Vector<Monster> mon_list = new Vector<>();
-	String pate = ""; 
+	String pate = "";
 	String mons[] = { "MonsterWolf", "MonsterBat", "MonsterOrc" };
 
 	void init() {
 		player.init();
+		player_list.add(player);
+		generateMonsters();
+	}
+
+	void addPlayer(Player newPlayer) {
+		player_list.add(newPlayer);
+	}
+
+	void removePlayer(Player playerToRemove) {
+		player_list.remove(playerToRemove);
+	}
+
+	void generateMonsters() {
+		for (String monsterName : mons) {
+			Monster monster = Monster.createMonster(monsterName);
+			if (monster != null) {
+				mon_list.add(monster); 
+			}
+		}
 	}
 
 }
