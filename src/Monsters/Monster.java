a7b784f883f20448ecb6fa41435a7e5c56ea0c8f
sungwoni9 <hurdang99@naver.com>
2024-11-17 23:48:02 +0900

    Add : createMonster and showStatus method

--- src/Monsters/Monster.java
+++ src/Monsters/Monster.java
@@ -1,10 +1,43 @@
 package Monsters;
 
 abstract public class Monster {
-	
-	  int curhp;
-	  int maxhp;
-	  int power;
-	  String name;
+
+	int curhp;
+	int maxhp;
+	int power;
+	String name;
+
+	public Monster(String name, int maxhp, int power) {
+		this.name = name;
+		this.maxhp = maxhp;
+		this.curhp = maxhp;
+		this.power = power;
+	}
+
+	public void takeDamage(int damage) {
+		this.curhp -= damage;
+		if (this.curhp < 0) {
+			this.curhp = 0; 
+		}
+	}
+
+	public static Monster createMonster(String monsterName) {
+		switch (monsterName) {
+		case "MonsterGolam":
+			return new MonsterGolam();
+		case "MonsterMush":
+			return new MonsterMush();
+		case "MonsterSnail":
+			return new MonsterSnail();
+		case "MonsterWolf":
+			return new MonsterWolf();
+		default:
+			return null;
+		}
+	}
+
+	public void showStatus() {
+		System.out.println(name + " - HP: " + curhp + "/" + maxhp + " Power: " + power);
+	}
 
 }
